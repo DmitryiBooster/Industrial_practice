79b5bfd63f3ea5378e43d41223c4d4a0db26843e
Dmitryi <dmitry@yandex.ru>
2025-12-19 15:45:48 +0300

    Инициализация проекта

---
+++ src/main/java/learning/spring/forproizvodsta/ForProizvodstaApplication.java
@@ -0,0 +1,13 @@
+package learning.spring.forproizvodsta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForProizvodstaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForProizvodstaApplication.class, args);
+    }
+
+}

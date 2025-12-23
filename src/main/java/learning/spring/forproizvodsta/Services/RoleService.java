2e960492a56f936817ece4df9034b46bcdf29e33
Dmitryi <dmitry@yandex.ru>
2025-12-23 18:28:26 +0300

    Добавление макета сервисов и описание их примерно функционала в комментариях

---
+++ src/main/java/learning/spring/forproizvodsta/Services/RoleService.java
@@ -0,0 +1,13 @@
+package learning.spring.forproizvodsta.Services;
+
+
+/**
+ * Какой должен быть функционал:
+ *
+ * - Получение ролей
+ * - Присвоение роли пользователю
+ * - Проверка прав пользователя
+ *  **/
+
+public class RoleService {
+}
